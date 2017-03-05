1b59780902c74368b90fb17d9cbf171f61659251
libo <libonero@didichuxing.com>
2017-03-05 15:31:36 +0800

    draw_thread3通过2个锁实现了不超发奖品

--- draw_thread3.py
+++ draw_thread3.py
@@ -3,10 +3,16 @@ import threading
 import time
 import MySQLdb
 import sys
+import redis
+import random
 
 '''
 要满足商品库存不会成负数都前提下，中奖记录也不会记录多发
 '''
+
+redis_client = redis.StrictRedis(host='127.0.0.1', port=6379)
+lottery_left_key = '1_left'
+redis_client.set(lottery_left_key, 2)
 
 class MyThread(threading.Thread):
     def __init__(self,arg):
@@ -16,6 +22,9 @@ class MyThread(threading.Thread):
         self.db=self.conn.cursor()
         self.conn.select_db('lottery')
 
+        self.redis = redis.StrictRedis(host='127.0.0.1', port=6379)
+        self.lottery_left_key = '1_left'
+
     def run(self):#定义每个线程要运行的函数
         time.sleep(1)
         uname = self.arg
@@ -23,18 +32,30 @@ class MyThread(threading.Thread):
         self.draw(id, uname)
 
     def draw(self, id, uname):
-        result = self.lock(id)
-        print uname + ', lock result is ' + bytes(result)
+        key = 'draw_'+bytes(id)+'_number_'
+	lock_key = self.lottery_left_key+'_lock'
+	lock_key_rs = self.redis.setex(lock_key, 5, uname)
+	print 'lock_key is ' + lock_key + ', lock_key_rs is ' + bytes(lock_key_rs)
+        left = self.redis.get(self.lottery_left_key)
+        left = int(left)
+	print 'left is ' + bytes(left)
+        if(left > 0 and lock_key_rs):
+            lock_rs = self.redis.setex(key+bytes(left), 5, uname)
+	    print 'key is ' + key + bytes(left) + ', lock_rs is ' + bytes(lock_rs)
+            if(lock_rs):
+                result = self.lock(id)
+                print uname + ', lock result is ' + bytes(result)
 
-        data = {'uname': uname, 'lottery_id': id}
-        result = self.record(data)
-        print uname + ', record result is ' + bytes(result)
-        '''
-        if(result):
-            data = {'uname': uname, 'lottery_id': id}
-            result = self.record(data)
-            print uname + ', record result is ' + bytes(result)
-        '''
+                data = {'uname': uname, 'lottery_id': id}
+		if(result):
+		    result = self.record(data)
+		    print uname + ', record result is ' + bytes(result)
+		    '''
+		    if(result):
+			data = {'uname': uname, 'lottery_id': id}
+			result = self.record(data)
+			print uname + ', record result is ' + bytes(result)
+		    '''
 
     #锁库存表
     def lock(self, id):
@@ -44,10 +65,14 @@ class MyThread(threading.Thread):
             result = self.db.execute('update lottery set amount=amount-1, updated=unix_timestamp(now()) where id = '+id+' and amount>=1')
             if(result):
                 self.conn.commit()
-            return 1
+	    self.redis.decr(self.lottery_left_key)
+	    lock_key = self.lottery_left_key+'_lock'
+	    self.redis.delete(lock_key)
+            print 'decr is '+bytes(self.redis.get(self.lottery_left_key))
+            return result
         except:
-                self.conn.rollback()
-                return 0
+            self.conn.rollback()
+            return 0
 
     #记录抽奖记录
     def record(self, data):
@@ -62,9 +87,10 @@ class MyThread(threading.Thread):
             self.conn.rollback()
             return 0
 
-users = ['黄厚生','郭巍','熊炜','陈锡乾'];
+#users = ['黄厚生','郭巍','熊炜','陈锡乾'];
 #users = ['黄厚生','郭巍','熊炜','陈锡乾','张梅珍','夏增明','陈敏章','徐亮'];
-#users = ['黄厚生','郭巍','熊炜','陈锡乾','张梅珍','夏增明','陈敏章','徐亮','陈德俊','朱磊','杨锐鹏','柯学','邓小华','李珉','郭春明','李金山','未写名','谢宏','章蛮','高平','好心人','时晶','林起泰'];
+users = ['黄厚生','郭巍','熊炜','陈锡乾','张梅珍','夏增明','陈敏章','徐亮','陈德俊','朱磊','杨锐鹏','柯学','邓小华','李珉','郭春明','李金山','未写名','谢宏','章蛮','高平','好心人','时晶','林起泰'];
+#random.shuffle(users)
 
 for i in xrange(len(users)):
     '''
